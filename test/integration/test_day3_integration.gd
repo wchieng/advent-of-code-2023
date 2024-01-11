515c830d3420b5dc69d5278fe9a2e0152b46264b
William Chieng <1075488+wchieng@users.noreply.github.com>
2024-01-11 01:11:13 -0800

    Fix resource paths in day 3 integration tests (#3)

--- test/integration/test_day3_integration.gd
+++ test/integration/test_day3_integration.gd
@@ -4,7 +4,7 @@ func test_get_potential_gear_positions():
 	var solver: Day3Solver = Day3Solver.new()
 	
 	# There should be 3 potential gears (* symbols) but two actual gears.
-	solver.init_grid_file("res://day3_example.txt")
+	solver.init_grid_file("res://day3/day3_example.txt")
 	var potential_gears: Array[Vector2] = solver.grid.get_potential_gear_positions()
 	assert_eq(potential_gears.size(), 3)
 	
@@ -14,7 +14,7 @@ func test_get_potential_gear_positions():
 func test_solve_example():
 	# The example should output 467835.
 	var solver: Day3Solver = Day3Solver.new()
-	solver.init_grid_file("res://day3_example.txt")
+	solver.init_grid_file("res://day3/day3_example.txt")
 	
 	var part1: int = solver.solve_part1()
 	assert_eq(part1, 4361)
