964b82194b35087f6cac67330535e1fc216519cf
William Chieng <williamchieng1991@gmail.com>
2024-01-11 18:08:46 -0800

    day 1 part 2 solution

--- day1/main_test.go
+++ day1/main_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMain_getCalibrationVal(t *testing.T) {
+func TestMain_getCalibrationValPart1(t *testing.T) {
 	type testCase struct {
 		Label       string
 		Input       string
@@ -47,7 +47,77 @@ func TestMain_getCalibrationVal(t *testing.T) {
 		},
 	}
 	for _, tc := range testCases {
-		actual, err := getCalibrationVal(tc.Input)
+		actual, err := getCalibrationValPart1(tc.Input)
+		assert.Equal(t, tc.ExpectedErr, err, tc.Label)
+		assert.Equal(t, tc.Expected, actual, tc.Label)
+	}
+}
+
+func TestMain_getCalibrationValPart2(t *testing.T) {
+	type testCase struct {
+		Label       string
+		Input       string
+		ExpectedErr error
+		Expected    int
+	}
+	testCases := []testCase{
+		{
+			Label:       "Empty line",
+			Input:       "",
+			ExpectedErr: fmt.Errorf("no calibration value in line"),
+			Expected:    0,
+		},
+		{
+			Label:       "First and last characters (only digits)",
+			Input:       "1abc2",
+			ExpectedErr: nil,
+			Expected:    12,
+		},
+		{
+			Label:       "Middle characters (only digits)",
+			Input:       "pqr3stu8vwx",
+			ExpectedErr: nil,
+			Expected:    38,
+		},
+		{
+			Label:       "Multiple digits, take only first and last (only digits)",
+			Input:       "a1b2c3d4e5f",
+			ExpectedErr: nil,
+			Expected:    15,
+		},
+		{
+			Label:       "Only one digit -> should be considered first and last (only digits)",
+			Input:       "treb7uchet",
+			ExpectedErr: nil,
+			Expected:    77,
+		},
+		{
+			Label:       "First and last characters (only alphabetical)",
+			Input:       "oneabctwo",
+			ExpectedErr: nil,
+			Expected:    12,
+		},
+		{
+			Label:       "Middle characters (only alphabetical)",
+			Input:       "pqrthreestueightvwx",
+			ExpectedErr: nil,
+			Expected:    38,
+		},
+		{
+			Label:       "Multiple digits, take only first and last (digits and alphabetical)",
+			Input:       "aoneb2cthreed4efivef",
+			ExpectedErr: nil,
+			Expected:    15,
+		},
+		{
+			Label:       "Only one number -> should be considered first and last (only alphabetical)",
+			Input:       "trebsevenuchet",
+			ExpectedErr: nil,
+			Expected:    77,
+		},
+	}
+	for _, tc := range testCases {
+		actual, err := getCalibrationValPart2(tc.Input)
 		assert.Equal(t, tc.ExpectedErr, err, tc.Label)
 		assert.Equal(t, tc.Expected, actual, tc.Label)
 	}
